493670f28a3cd7f34249e2095a8e92477e115cd7
Yangheng Chen <11018737@live.mercer.edu>
2023-02-02 19:26:13 +0000

    Upload New File

---
+++ fork1/funky.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(int argc, char** argv) {
+
+
+
+    int N = atoi(argv[1]);
+    int child = 0;
+    int i;
+    if(N!= 0)
+    {
+        child =1;
+    }
+
+    for (i=0; i<child; i++) {
+	      if (fork() == 0) {
+            child++;
+            if (child > N){
+		            break;
+            }
+            i=-1;
+            
+        }
+    }
+
+
+  printf("%d\t%d\n", getpid(), getppid());
+	sleep(1);    
+	return 0;
+}
+
