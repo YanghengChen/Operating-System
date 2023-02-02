24b9333fcba3cb9737300d07919aceb4fcee1077
Yangheng Chen <11018737@live.mercer.edu>
2023-02-02 20:01:46 +0000

    Replace funky.c

--- fork1/funky.c
+++ fork1/funky.c
@@ -8,19 +8,22 @@ int main(int argc, char** argv) {
     int N = atoi(argv[1]);
     int child = 0;
     int i;
+    if(  N<0 || N >5 ){
+        printf("Input out of range \n");
+        return 1;
+    }
     if(N!= 0)
     {
         child =1;
     }
 
     for (i=0; i<child; i++) {
-	      if (fork() == 0) {
+	if (fork() == 0) {
             child++;
-            if (child > N){
-		            break;
+	    if (child > N){
+		 break;
             }
-            i=-1;
-            
+            i= -1;
         }
     }
 
