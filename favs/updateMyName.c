2186223cc9026cca46bf25f603b5d396ac8e6b7f
Yangheng Chen <11018737@live.mercer.edu>
2023-02-25 18:00:10 +0000

    Upload New File

---
+++ favs/updateMyName.c
@@ -0,0 +1,39 @@
+#include <sys/types.h>
+#include <sys/ipc.h>
+#include <sys/shm.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+typedef struct {
+    int id;
+    char name[20];
+    int favNum;
+    char favFood[30];
+} bStruct;
+
+int main(int argc, char **argv) {
+    if (argc == 1) {
+        printf("Type in something \n");
+        return 0;
+    }
+
+    FILE *fopen(), *fp;
+    int shmid;
+    if ((fp = fopen("/pub/csc460/bb/BBID.txt","r")) == NULL) {
+        printf("Cannot open the file.\n");
+        return 0;
+    }
+    fscanf(fp,"%d",&shmid);
+
+    bStruct *shmem;
+    if ((shmem = (bStruct *) shmat(shmid, NULL, SHM_RND)) == (void *)-1) {
+        printf("Cannot attach shared memory.\n");
+         return 0;
+    }
+
+    // I am number 4 (index 3) on the list.
+    shmem += 3;
+    strncpy(shmem->name, argv[1], 19);
+    return 0;
+}
